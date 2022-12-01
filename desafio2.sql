d804c2d7c8c3fc31f612e1eeff495fd32ecc8b63
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-12-01 15:10:45 -0300

    feat: requisito 2 - finalizado

--- desafio2.sql
+++ desafio2.sql
@@ -5,17 +5,18 @@ SELECT COUNT(*) AS cancoes,
 FROM SpotifyClone.tb_cancoes;
 */
 
+/*
 SELECT COUNT(cancao_id) AS cancoes,
   (SELECT COUNT(artista_id) FROM SpotifyClone.tb_artistas) AS artistas,
   (SELECT COUNT(album_id) FROM SpotifyClone.tb_albuns) AS albuns
 FROM SpotifyClone.tb_cancoes;
+*/
 
-/*
+
 SELECT 
   COUNT(c.nome) AS cancoes, 
-  COUNT(ar.nome) AS artistas, 
-  COUNT(al.nome) AS albuns 
+  COUNT(DISTINCT ar.nome) AS artistas, 
+  COUNT(DISTINCT al.nome) AS albuns 
 FROM SpotifyClone.tb_cancoes c
 INNER JOIN SpotifyClone.tb_albuns al ON c.album_id = al.album_id
 INNER JOIN SpotifyClone.tb_artistas ar ON  al.artista_id = ar.artista_id;
-*/
