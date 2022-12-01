f86edd15f46ef26fb85cd172b5484ee0b059567a
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-12-01 16:14:44 -0300

    feat: requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT
+  c.nome AS nome,
+  COUNT(r.usuario_id) AS reprocucoes 
+FROM SpotifyClone.tb_cancoes c
+  INNER JOIN SpotifyClone.tb_reproducoes r ON r.cancao_id = c.cancao_id
+  INNER JOIN SpotifyClone.tb_usuarios u ON u.usuario_id = r.usuario_id
+WHERE u.plano_id IN (1, 4)
+GROUP BY nome
+ORDER BY nome;
