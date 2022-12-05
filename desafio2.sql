a3427612d283cbec826ce6cca994d799103e6710
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-12-05 19:17:37 -0300

    fix: consertando erro

--- desafio2.sql
+++ desafio2.sql
@@ -1,16 +1,11 @@
-/*
-SELECT COUNT(*) AS cancoes,
-  (SELECT COUNT(nome) FROM SpotifyClone.tb_artistas) AS artistas,
-  (SELECT COUNT(nome) FROM SpotifyClone.tb_albuns) AS albuns
-FROM SpotifyClone.tb_cancoes;
-*/
-
-
 -- SELECT COUNT(cancao_id) AS cancoes,
 --   (SELECT COUNT(artista_id) FROM SpotifyClone.tb_artistas) AS artistas,
 --   (SELECT COUNT(album_id) FROM SpotifyClone.tb_albuns) AS albuns
 -- FROM SpotifyClone.tb_cancoes;
 
+-- SELECT (SELECT COUNT(cancao_id) FROM SpotifyClone.tb_cancoes) AS cancoes,
+-- (SELECT COUNT(artista_id) FROM SpotifyClone.tb_artistas) AS artistas,
+-- (SELECT COUNT(album_id) FROM SpotifyClone.tb_albuns) AS albuns;
 
 SELECT 
   COUNT(c.nome) AS cancoes, 
@@ -19,7 +14,3 @@ SELECT
 FROM SpotifyClone.tb_cancoes c
 INNER JOIN SpotifyClone.tb_albuns al ON c.album_id = al.album_id
 INNER JOIN SpotifyClone.tb_artistas ar ON  al.artista_id = ar.artista_id;
-
--- SELECT (SELECT COUNT(cancao_id) FROM SpotifyClone.tb_cancoes) AS cancoes,
--- (SELECT COUNT(artista_id) FROM SpotifyClone.tb_artistas) AS artistas,
--- (SELECT COUNT(album_id) FROM SpotifyClone.tb_albuns) AS albuns;
